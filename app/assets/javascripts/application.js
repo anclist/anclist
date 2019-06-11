47dc8e01f829945eb09e00c4a84055a6e6cc5c3f
Tony <ANCLIST@GMAIL.COM>
2019-06-11 18:06:15 -0400

    Scrolling effect to tech syack cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,3 +49,55 @@ $(window).scroll(function(e) {
 });
 
 // End of scroll functions navbar disapear when scrolling
+
+// Tech stack cards slide up as user scrolls
+(function($) {
+  $.fn.visible = function(partial) {
+
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  };
+
+})(jQuery);
+
+$(window).scroll(function(event) {
+
+  $(".card").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
+  });
+
+});
+
+var win = $(window);
+var allMods = $(".card");
+
+// Already visible modules
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+win.scroll(function(event) {
+
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
+  });
+
+});
+// End of Tech stack cards slide up as user scrolls
