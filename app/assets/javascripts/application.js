65fdf3f8207028f3eb30b5dbe076b2dcf3fbe58e
Tony <ANCLIST@GMAIL.COM>
2019-06-15 10:55:01 -0400

    Put all JS scripts inside the document ready function, so the social navbar toggle works every time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,72 +20,76 @@
 
 // Navbar fades color when scrolling
 
-$(window).scroll(function(e) {
+$(document).ready(function() {
 
-    // add/remove class to navbar when scrolling to hide/show
-    var scroll = $(window).scrollTop();
-    if (scroll >= 2) {
-        $('#second-navbar').addClass("navbar-fade");
-    } else {
-        $('#second-navbar').removeClass("navbar-fade");
-    }
+ $(window).scroll(function(e) {
 
-});
+  // add/remove class to navbar when scrolling to hide/show
+  var scroll = $(window).scrollTop();
+  if (scroll >= 2) {
+   $('#second-navbar').addClass("navbar-fade");
+  } else {
+   $('#second-navbar').removeClass("navbar-fade");
+  }
 
-// End of Navbar fades color when scrolling
+ });
 
-// Jquey smooth scrolling
+ // End of Navbar fades color when scrolling
 
-// Select all links with hashes
-$('a[href*="#"]')
+ // Jquey smooth scrolling
+
+ // Select all links with hashes
+ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
+   // On-page links
+   if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    location.hostname == this.hostname
+   ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+     // Only prevent default if animation is actually gonna happen
+     event.preventDefault();
+     $('html, body').animate({
+      scrollTop: target.offset().top
+     }, 1000, function() {
+      // Callback after animation
+      // Must change focus!
+      var $target = $(target);
+      $target.focus();
+      if ($target.is(":focus")) { // Checking if the target was focused
+       return false;
+      } else {
+       $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+       $target.focus(); // Set focus again
+      };
+     });
     }
+   }
   });
-// End of Jquey smooth scrolling
+ // End of Jquey smooth scrolling
 
-$(document).ready(function(){
-  var socialToggle = $("#social-toggle");
-  var firstNavbar = $("#fisrt-navbar");
-  var closeSocial = $("#close-social-icons");
+ // show or hide social media navbar
+
+ var socialToggle = $("#social-toggle");
+ var firstNavbar = $("#fisrt-navbar");
+ var closeSocial = $("#close-social-icons");
 
 
-  socialToggle.click(function(e){
-    e.preventDefault();
-    firstNavbar.toggleClass("show-side-bar");
-  });
-  closeSocial.click(function(e){
-    e.preventDefault();
-    firstNavbar.removeClass("show-side-bar");
-  });
+ socialToggle.click(function(e) {
+  e.preventDefault();
+  firstNavbar.toggleClass("show-side-bar");
+ });
+ closeSocial.click(function(e) {
+  e.preventDefault();
+  firstNavbar.toggleClass("show-side-bar");
+ });
+ // End of show or hide social media navbar
+
 });
